322779ded495b886ddb1bc05167b3b30746ecec1
Ferdinand Shiloh Hart <shilohwrpz@gmail.com>
2026-05-10 14:48:20 +0200

    fix: remove deleted pm2_manager import from utils/__init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,2 @@
 from .logger import get_logger
-from .pm2_manager import spawn_watcher, kill_watcher, watcher_status
 from .time_utils import now_wat, today_str, yesterday_str, weekday_str, ago_since
